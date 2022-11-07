b8d88c44397b09cb80c1faecc88c1588c959a14f
Ofer Tauber <ofertauber@gmail.com>
2022-11-07 15:28:00 +0200

    init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 5000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.listen(PORT).then(() => {
+    console.log(`Server is up and listening to port ${PORT}`);
+  });
 }
 bootstrap();
