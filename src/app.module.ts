46b3ed6bb8ca0ab1f7d19463628966354432d44f
Ofer Tauber <ofertauber@gmail.com>
2022-11-07 15:42:25 +0200

    add react

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { resolve } from 'path';
+
+const pathToReact = resolve(__dirname, '../client/build');
+console.log(pathToReact);
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: pathToReact,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
