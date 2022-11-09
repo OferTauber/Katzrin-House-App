7344854ee19406b239b4e1637cf6b31ba834455b
Ofer Tauber <ofertauber@gmail.com>
2022-11-09 12:48:27 +0200

    Re-install react as Typescript

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserIsLoged from './components/loged_user/UserIsLoged';
+
+function App() {
+  return (
+    <>
+      <UserIsLoged />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/loged_user/UserIsLoged.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from './Nav';
+import Calendar from './calendar/Calendar';
+
+const UserIsLoged = () => {
+  return (
+    <>
+      <Nav />
+      <Calendar />
+    </>
+  );
+};
+
+export default UserIsLoged;
